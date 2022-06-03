009c2ebdc04deefaf12b5d7a878e45e55cfa568f
Fahad Shafique <fahadshafique1994@gmail.com>
2022-06-04 02:06:02 +0500

    seeder added for RBAC

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,58 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+    $user=  User::create([
+            'name' => 'Admin',
+            'user_name' => 'Admin',
+            'email'=>'admin@test.com',
+            'password'=>bcrypt('admin123'),
+
+        ]);
+        Permission::create(
+            [
+            'name'=>'view_dashboard',
+                ],
+            [
+            'name'=>'manage_dasboard',
+            ],
+            [
+                'name'=>'view_users',
+            ],
+            [
+                'name'=>'manage_users',
+            ],
+            [
+                'name'=>'view_roles',
+            ],
+            [
+                'name'=>'manage_roles',
+            ],
+            [
+                'name'=>'view_permissions',
+            ],
+            [
+                'name'=>'manage_permissions',
+            ],
+            [
+                'name'=>'view_sports',
+            ],
+            [
+                'name'=>'manage_sports',
+            ],
+            [
+                'name'=>'view_leagues',
+            ],
+            [
+                'name'=>'manage_leagues',
+            ]
+        );
+        $role = Role::create(['name' => 'super-admin']);
+
+        $permissions = Permission::pluck('id','id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+
     }
 }
