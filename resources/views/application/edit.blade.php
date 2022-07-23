699742329334158fa71b9ab06a55c907db49b687
fahadshafique786 <60979429+fahadshafique786@users.noreply.github.com>
2022-07-24 03:15:58 +0500

    fixed number validation in application form!

--- resources/views/application/edit.blade.php
+++ resources/views/application/edit.blade.php
@@ -327,6 +327,18 @@
 
         $(document).ready(function($){
 
+
+            $('#adsIntervalTime,#minimumVersionSupport').keypress(function (e) {
+
+                var charCode = (e.which) ? e.which : event.keyCode
+
+                if (String.fromCharCode(charCode).match(/[^0-9+.]/g))
+
+                    return false;
+
+            });
+
+
             var Toast = Swal.mixin({
                 toast: true,
                 position: 'top-end',
