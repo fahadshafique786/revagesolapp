344fd4f817994c062811e892458885d39e9a8673
fahadshafique786 <60979429+fahadshafique786@users.noreply.github.com>
2022-07-25 22:33:52 +0500

    app credentials screen design and fetch data in the datable

--- app/Http/Controllers/AppCredentialsController.php
+++ app/Http/Controllers/AppCredentialsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\AppCredentials;
 use Illuminate\Http\Request;
 use App\Models\AppDetails;
+use Response;
 
 class AppCredentialsController extends Controller
 {
